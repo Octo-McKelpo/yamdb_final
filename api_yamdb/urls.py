6ded40ee5573025f7ed34ec6791b6f6fe5bd16de
Octo-McKelpo <octopush1997@gmail.com>
2021-08-20 14:29:31 +0300

    3d commit

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -16,14 +16,33 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YamDB API",
+        default_version='v1',
+        description="Документация для приложения YamDB API",
+        contact=openapi.Contact(email="admin@yandex.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+]
+
+urlpatterns += [
+    url(
+        r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    )
 ]
