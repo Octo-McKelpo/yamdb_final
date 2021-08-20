6e8a78bb5340d0587e0c139cf836785629c89962
Octo-McKelpo <octopush1997@gmail.com>
2021-08-20 14:31:06 +0300

    3d commit

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf.urls import url
-
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
